c249aba520e5dd4b18a937f9e5238ef14ff53159
Taís Doico <taisdoico@yahoo.com.br>
2026-06-01 17:07:44 -0300

    Atualizando o código e o README

--- notebooks/base_varejo.py
+++ notebooks/base_varejo.py
@@ -32,12 +32,12 @@ pct = round(nulos_por_coluna / len(df) * 100, 2)
 print(pd.DataFrame({'Nulos': nulos_por_coluna, '% Nulos': pct}))
 
 # Substituindo usando estruturas condicionais
-if "categoria" in df.columns:
-    df['categoria'] = df['categoria'].fillna('Sem categoria')
+if "CATEGORIA" in df.columns:
+    df['CATEGORIA'] = df['CATEGORIA'].fillna('Sem cATEGORIA')
 
-# Usando condicional para preencher valores nulos 
-if "valor" in df.columns:
-    df['valor'] = df['valor'].fillna(df['valor'].mean())
+# Usando condicional para preencher valores nulosvazios 
+if "CL_SEG" in df.columns:
+    df['CL_SEG'] = df['CL_SEG'].fillna('Não Informado')
 
 # Eliminando colunas com valores nulos
 colunas_para_eliminar = ['Unnamed: 10', 'Unnamed: 11', 'Unnamed: 12', 'Unnamed: 13']
@@ -49,8 +49,8 @@ total_duplicatas = df.duplicated().sum()
 print(f'Total de registros duplicados: {total_duplicatas}')
 
 # Encontrando e eliminando duplicatas
-df_sem_duplicatas = df.drop_duplicates()
-print(f'Total de registros após eliminar duplicatas: {df_sem_duplicatas.shape[0]}')
+df.drop_duplicates(inplace=True)
+print(f'Total de registros após eliminar duplicatas: {df.shape[0]}')
 
 
 # Codigo de conversão para Datatime
@@ -62,26 +62,27 @@ if 'DATA' in df.columns:
 if 'CL_FHL' in df.columns:
     print(df['CL_FHL'].describe())
 
-media_fhl = df['CL_FHL'].mean()
-print(f'Média de filhos: {media_fhl}')
-mediana_fhl = df['CL_FHL'].median()
-print(f'Mediana de filhos: {mediana_fhl}')
-desvio_padrao_fhl = df['CL_FHL'].std()
-print(f'Desvio padrão de filhos: {desvio_padrao_fhl}')
-moda_fhl = df['CL_FHL'].mode()[0]
-print(f'Moda de filhos: {moda_fhl}')
-min_fhl = df['CL_FHL'].min()
-print(f'Mínimo de filhos: {min_fhl}')
-max_fhl = df['CL_FHL'].max()
-print(f'Máximo de filhos: {max_fhl}')
+    media_fhl = df['CL_FHL'].mean()
+    print(f'Média de filhos: {media_fhl}')
+    mediana_fhl = df['CL_FHL'].median()
+    print(f'Mediana de filhos: {mediana_fhl}')
+    desvio_padrao_fhl = df['CL_FHL'].std()
+    print(f'Desvio padrão de filhos: {desvio_padrao_fhl}')
+    moda_fhl = df['CL_FHL'].mode()[0]
+    print(f'Moda de filhos: {moda_fhl}')
+    min_fhl = df['CL_FHL'].min()
+    print(f'Mínimo de filhos: {min_fhl}')
+    max_fhl = df['CL_FHL'].max()
+    print(f'Máximo de filhos: {max_fhl}')
 
 
 # Explorando padrões de agrupamento
 colunas_para_agrupamento = ['PR_CAT', 'PR_NOME']
 agrupamento = df.groupby(colunas_para_agrupamento).size().reset_index(name='Contagem')
-agrupamento.sort_values(by='Contagem', ascending=False)
+agrupamento.sort_values(by='Contagem', ascending=False, inplace=True)
 print(agrupamento.head(10))
 
 
 # Salvando o dataset limpo
 df.to_csv('Base_Varejo_Limpo.csv', index=False, sep=';', encoding='utf-8')
+print('Dataset limpo salvo como "Base_Varejo_Limpo.csv".')
